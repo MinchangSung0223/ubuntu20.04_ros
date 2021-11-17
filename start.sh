d0b0a51c046672b5a3826db30cde233afa83ea20
tjdalsckd <53217819+tjdalsckd@users.noreply.github.com>
2021-11-17 23:00:19 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 xhost +local:root
+docker rm -f ros
  docker  run  --rm -it --name ros --privileged  --volume=/dev:/dev --gpus all --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -e QT_X11_NO_MITSHM=1 tjdalsckd/ros2:latest bash 
